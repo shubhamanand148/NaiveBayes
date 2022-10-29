f23fed007a4ebaf471313ec3a8c35ea36bf33f2f
Shubham Anand <shubhamanand148@gmail.com>
2022-10-29 17:06:14 +0530

    This is Sample Naive Bayes

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd, numpy as np, sklearn.preprocessing as pp
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split, cross_val_score
+
+credit_data = pd.read_csv("C:\Code\Datasets\credit_data.csv")
+x = np.array(credit_data[["income", "age", "loan"]]).reshape(-1, 3)
+y = np.array(credit_data.default)
+
+x = pp.MinMaxScaler.fit_transform(x)
+
+model = GaussianNB()
+scores = cross_val_score(model, x, y, cv=10, scoring='accuracy')
+print("Accuracy Score: ", scores.mean())
